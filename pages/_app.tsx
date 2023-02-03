1fead5b2cf44200754377d153dd89d46c7576cab
吕学亮 <lvxl@shanhs.com>
2023-02-03 18:38:03 +0800

    clear

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import "../styles/global.css";
-import 'antd/dist/antd.css';
-import { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
